0b828e442b8fafa9f50191f32991b70b0d295d91
Animesh <animesh.nauti@gmail.com>
2019-05-24 22:32:31 +0530

    updated urls.py

--- communitygis/urls.py
+++ communitygis/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include, re_path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.core import urls as wagtail_urls
@@ -27,4 +30,4 @@ urlpatterns = [
     re_path(r'^cms/', include(wagtailadmin_urls)),
     re_path(r'^documents/', include(wagtaildocs_urls)),
     re_path(r'^pages/', include(wagtail_urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
