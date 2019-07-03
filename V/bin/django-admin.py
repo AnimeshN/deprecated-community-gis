29a2547578adad342fd74dc626bda53a23e51f05
Dhiraj Vishwakarma <dhiraj7894@gmail.com>
2019-07-04 00:33:25 +0530

    Link and missing data updated

---
+++ V/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dhiraj_gt/Desktop/Community-GIS/community-gis/V/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ V/lib/python3.6/site-packages/wagtail/utils/deprecation.py
@@ -0,0 +1,9 @@
+class RemovedInWagtail26Warning(DeprecationWarning):
+    pass
+
+
+removed_in_next_version_warning = RemovedInWagtail26Warning
+
+
+class RemovedInWagtail27Warning(PendingDeprecationWarning):
+    pass
